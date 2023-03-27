168e5d76137518b88d69b6ed35046bcf5387bf54
fchahid2023 <fchahid2023@129SE2-DD4MMS3.ad.campus-eni.fr>
2023-03-27 16:35:58 +0200

    [bo] V2 ajout classe ArticleVendu

--- src/main/java/fr/eni/trocenchere/bo/ArticleVendu.java
+++ src/main/java/fr/eni/trocenchere/bo/ArticleVendu.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 
 public class ArticleVendu {
 	
-	private Integer noArticle;
-	private String nomArticle;
+	private Integer no_article;
+	private String nom_article;
 	private String description;
 	private LocalDate debutEnchere;
 	private LocalDate finEnchere;
-	private Integer prixInitial ;
-	private Integer prixFinal ;
-	private Integer noUtilisateur; 
-	private Integer noCategorie;
+	private Integer prix_initial ;
+	private Integer prix_final ;
+	private Integer no_utilisateur; 
+	private Integer no_categorie;
 	
 
 	
@@ -21,33 +21,34 @@ public class ArticleVendu {
 	}
 
 
-	public ArticleVendu(Integer noArticle, String nomArticle, String description, LocalDate debutEnchere,
-			LocalDate finEnchere, Integer prixInitial, Integer prixFinal, Integer noUtilisateur, Integer noCategorie) {
+	public ArticleVendu(Integer no_article, String nom_article, String description, LocalDate debutEnchere,
+			LocalDate finEnchere, Integer prix_initial, Integer prix_final, Integer no_utilisateur,
+			Integer no_categorie) {
 		super();
-		this.noArticle = noArticle;
-		this.nomArticle = nomArticle;
+		this.no_article = no_article;
+		this.nom_article = nom_article;
 		this.description = description;
 		this.debutEnchere = debutEnchere;
 		this.finEnchere = finEnchere;
-		this.prixInitial = prixInitial;
-		this.prixFinal = prixFinal;
-		this.noUtilisateur = noUtilisateur;
-		this.noCategorie = noCategorie;
+		this.prix_initial = prix_initial;
+		this.prix_final = prix_final;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
 	}
 
 
-	public Integer getNoArticle() {
-		return noArticle;
+	public Integer getNo_article() {
+		return no_article;
 	}
-	public void setNoArticle(Integer noArticle) {
-		this.noArticle = noArticle;
+	public void setNo_article(Integer no_article) {
+		this.no_article = no_article;
 	}
 
-	public String getNomArticle() {
-		return nomArticle;
+	public String getNom_article() {
+		return nom_article;
 	}
-	public void setNomArticle(String nomArticle) {
-		this.nomArticle = nomArticle;
+	public void setNom_article(String nom_article) {
+		this.nom_article = nom_article;
 	}
 
 	public String getDescription() {
@@ -70,44 +71,45 @@ public class ArticleVendu {
 	public void setFinEnchere(LocalDate finEnchere) {
 		this.finEnchere = finEnchere;
 	}
-
-	public Integer getPrixInitial() {
-		return prixInitial;
+	
+	public Integer getPrix_initial() {
+		return prix_initial;
 	}
-	public void setPrixInitial(Integer prixInitial) {
-		this.prixInitial = prixInitial;
-	}
-
-	public Integer getPrixFinal() {
-		return prixFinal;
-	}
-	public void setPrixFinal(Integer prixFinal) {
-		this.prixFinal = prixFinal;
+	public void setPrix_initial(Integer prix_initial) {
+		this.prix_initial = prix_initial;
 	}
 
-	public Integer getNoUtilisateur() {
-		return noUtilisateur;
+	public Integer getPrix_final() {
+		return prix_final;
 	}
-	public void setNoUtilisateur(Integer noUtilisateur) {
-		this.noUtilisateur = noUtilisateur;
+	public void setPrix_final(Integer prix_final) {
+		this.prix_final = prix_final;
 	}
 
-	public Integer getNoCategorie() {
-		return noCategorie;
+	public Integer getNo_utilisateur() {
+		return no_utilisateur;
 	}
-	public void setNoCategorie(Integer noCategorie) {
-		this.noCategorie = noCategorie;
+	public void setNo_utilisateur(Integer no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+
+	public Integer getNo_categorie() {
+		return no_categorie;
+	}
+	public void setNo_categorie(Integer no_categorie) {
+		this.no_categorie = no_categorie;
 	}
 
 
 	@Override
 	public String toString() {
-		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
-				+ ", debutEnchere=" + debutEnchere + ", finEnchere=" + finEnchere + ", prixInitial=" + prixInitial
-				+ ", prixFinal=" + prixFinal + ", noUtilisateur=" + noUtilisateur + ", noCategorie=" + noCategorie
-				+ "]";
+		return "ArticleVendu [no_article=" + no_article + ", nom_article=" + nom_article + ", description="
+				+ description + ", debutEnchere=" + debutEnchere + ", finEnchere=" + finEnchere + ", prix_initial="
+				+ prix_initial + ", prix_final=" + prix_final + ", no_utilisateur=" + no_utilisateur + ", no_categorie="
+				+ no_categorie + "]";
 	}
-	
+
+
 	
 	
 	
