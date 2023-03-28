d20b1e4d145b0357ea35767d04e93916d2c2bf48
flesperant2023 <flesperant2023@129SE2-861MMS3.ad.campus-eni.fr>
2023-03-28 12:29:14 +0200

    [methodes] affichage methodes Insert, Delete, Update

--- src/main/java/fr/eni/trocenchere/dal/EncheresDAOJdbcImpl.java
+++ src/main/java/fr/eni/trocenchere/dal/EncheresDAOJdbcImpl.java
@@ -8,11 +8,30 @@ import fr.eni.trocenchere.bo.Utilisateur;
 
 public class EncheresDAOJdbcImpl implements EncheresDAO {
 
+	private static final String INSERT_UTILISATEUR = "INSERT INTO utilisateur (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) "
+													+"VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+	private static final String INSERT_ARTICLEVENDU = "INSERT INTO articlevendu (nom, description, date_debut_enchere, date_fin_enchere, prix_initial, prix_vente, Utilisateur_no_utilisateur, Categorie_no_categeorie) "
+													+ "VALUES (?,?,?,?,?,?,?,?)";
+	
+	private static final String INSERT_ENCHERE = "INSERT INTO enchere (date_enchere, montant_enchere, Utilisateur_no_utilisateur, ArticleVendu_no_article) VALUES(?,?,?,?)";
+	
+	private static final String INSERT_RETRAIT = "INSERT INTO retrait (rue, code_postal, ville, ArticleVendu_no_article) VALUES(?,?,?,?)";
+	
+	private static final String INSERT_CATEGORIE = "INSERT INTO categorie (libelle) VALUES (?) ";
+	
+	private static final String DELETE_UTILISATEUR = "DELETE FROM utilisateur (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) WHERE no_utilisateur";
+	
+	private static final String DELETE_ARTICLEVENDU = "DELETE FROM articlevendu (no_article, nom, description, date_debut_enchere, date_fin_enchere, prix_initial, prix_vente, Utilisateur_no_utilisateur, Categorie_no_categeorie)WHERE no_article";
+			
+	private static final String UPDATE_UTILISATEUR = "UPDATE utilisateur SET (pseudo=?, nom=?, prenom=?, email=?, telephone=?, rue=?, code_postal=?, ville=?, mot_de_passe=?, credit=?, administrateur=?) WHERE no_utilisateur";
+
+	private static final String UPDATE_ENCHERE = "UPDATE enchere SET (date_enchere=?, montant_enchere=?, Utilisateur_no_utilisateur=?, ArticleVendu_no_article=?) WHERE ArticleVendu_no_article";
+	
+	private static final String SELECT_UTILISATEUR = "INSERT INTO utilisateur (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) "
 	
 	
 	
-	
-	
+
 	@Override
 	public void insert_utilisateur(Utilisateur utilisateur) throws BusinessException {
 		// TODO Auto-generated method stub
