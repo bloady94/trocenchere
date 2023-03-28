0cc2c53be010a362342c91bc14c2784f971674b8
fchahid2023 <fchahid2023@129SE2-DD4MMS3.ad.campus-eni.fr>
2023-03-28 14:49:51 +0200

    [ ] INSERT ARTICLEVENDU

--- src/main/java/fr/eni/trocenchere/dal/EncheresDAOJdbcImpl.java
+++ src/main/java/fr/eni/trocenchere/dal/EncheresDAOJdbcImpl.java
@@ -1,5 +1,10 @@
 package fr.eni.trocenchere.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import fr.eni.trocenchere.BusinessException;
 import fr.eni.trocenchere.bo.ArticleVendu;
 import fr.eni.trocenchere.bo.Enchere;
@@ -40,10 +45,32 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 	}
 
 	@Override
-	public void insert_articleVendu(ArticleVendu articleVendu) throws BusinessException {
-		// TODO Auto-generated method stub
-
+	public void insert_articleVendu(ArticleVendu articleVendu) throws BusinessException{
+		
+		try (Connection con = ConnectionProvider.getConnection()) {
+			PreparedStatement pstmt = con.prepareStatement(INSERT_ARTICLEVENDU, PreparedStatement.RETURN_GENERATED_KEYS);
+		
+			
+			pstmt.setString(1, articleVendu.getNom_article());
+			pstmt.setString(2, articleVendu.getDescription());
+			pstmt.setDate(3, java.sql.Date.valueOf(articleVendu.getDebutEnchere()));
+			pstmt.setDate(4, java.sql.Date.valueOf(articleVendu.getFinEnchere()));
+			pstmt.setInt(5,  articleVendu.getPrix_initial());
+			pstmt.setInt(6,  articleVendu.getPrix_final());
+			pstmt.setInt(7,  articleVendu.getNo_utilisateur());
+			pstmt.setInt(8,  articleVendu.getNo_categorie());
+			pstmt.executeUpdate();
+			
+			ResultSet rs = pstmt.getGeneratedKeys();		
+			if(rs.next()) {
+				articleVendu.setNo_article(rs.getInt(1));
+			}
+		} catch (Exception  e) {
+			e.printStackTrace();
+		}
 	}
+			
+		
 
 	@Override
 	public void insert_enchere(Enchere enchere) throws BusinessException {
