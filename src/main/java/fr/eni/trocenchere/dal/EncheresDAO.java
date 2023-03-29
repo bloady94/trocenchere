fa1a52e174d9cf3f81180e3fc6b6aa74ed910c7f
alefebvre2023 <alefebvre2023@129SE2-451MMS3.ad.campus-eni.fr>
2023-03-29 11:51:25 +0200

    modification de l'ordre des fonctions

--- src/main/java/fr/eni/trocenchere/dal/EncheresDAO.java
+++ src/main/java/fr/eni/trocenchere/dal/EncheresDAO.java
@@ -9,21 +9,24 @@ import fr.eni.trocenchere.bo.Utilisateur;
 
 public interface EncheresDAO {
 	
-	public void insert_utilisateur(Utilisateur utilisateur) throws BusinessException;
-	public void insert_articleVendu(ArticleVendu articleVendu) throws BusinessException;
-	public void insert_enchere(Enchere enchere) throws BusinessException;
 	public void insert_retrait(Retrait retrait) throws BusinessException;
+	
+	
 	public void insert_categorie(Categorie categorie) throws BusinessException;
 	
-	
-	public void delete_utilisateur(Utilisateur utilisateur) throws BusinessException;
-	public void delete_articleVendu(ArticleVendu articleVendu) throws BusinessException;
-	 
-	
-	public void update_utilisateur(Utilisateur utilisateur) throws BusinessException;
-	public void update_enchere(Enchere enchere) throws BusinessException;
 
+	public void insert_utilisateur(Utilisateur utilisateur) throws BusinessException;
+	public void update_utilisateur(Utilisateur utilisateur) throws BusinessException;
 	public void select_utilisateur(Utilisateur utilisateur) throws BusinessException;
+	public void delete_utilisateur(Utilisateur utilisateur) throws BusinessException;
+
+
+	public void insert_articleVendu(ArticleVendu articleVendu) throws BusinessException;
 	public void select_article(ArticleVendu articleVendu) throws BusinessException;
+	public void delete_articleVendu(ArticleVendu articleVendu) throws BusinessException;
+	
+	public void update_enchere(Enchere enchere) throws BusinessException;
+	public void insert_enchere(Enchere enchere) throws BusinessException;
+
 
 }
