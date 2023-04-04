1b5d4c43f907f8dac10cfc5c33a0463e95d9a288
flesperant2023 <flesperant2023@129SE2-861MMS3.ad.campus-eni.fr>
2023-04-04 10:03:28 +0200

    [MODICATION] Mise à jour de la bll et de l'interface pour modifierProfil

--- src/main/java/fr/eni/trocenchere/bll/ModifierProfil.java
+++ src/main/java/fr/eni/trocenchere/bll/ModifierProfil.java
@@ -6,5 +6,5 @@ import fr.eni.trocenchere.bo.Utilisateur;
 public interface ModifierProfil {
 	
 	public Utilisateur UpdateUtilisateur(Utilisateur user, String pseudo, String nom, String prenom, String email, String telephone,
-			String rue, String codePostal, String ville, String motDePasse) throws BusinessException ;
-}
+			String rue, String codePostal, String ville, String mdpActuel, String nouveauMDP) throws BusinessException ;
+	}
--- src/main/java/fr/eni/trocenchere/bll/ModifierProfilManager.java
+++ src/main/java/fr/eni/trocenchere/bll/ModifierProfilManager.java
@@ -9,7 +9,7 @@ import fr.eni.trocenchere.dal.DAOFactory;
 import fr.eni.trocenchere.dal.ModifierProfilDAO;
 
 public class ModifierProfilManager implements ModifierProfil {
-	
+
 	private ModifierProfilDAO modifierProfilDAO;
 
 	public ModifierProfilManager() {
@@ -18,104 +18,127 @@ public class ModifierProfilManager implements ModifierProfil {
 
 	@Override
 	public Utilisateur UpdateUtilisateur(Utilisateur user, String pseudo, String nom, String prenom, String email,
-			String telephone, String rue, String codePostal, String ville, String motDePasse) throws BusinessException {
+			String telephone, String rue, String codePostal, String ville, String mdpActuel, String nouveauMDP) throws BusinessException {
 
 		BusinessException businessException = new BusinessException();
-		
+
 		this.validerPseudo(pseudo, businessException);
 		this.validerEmail(email, businessException);
-		this.validerCoordonnees(pseudo,nom, prenom, telephone, rue, codePostal, ville, motDePasse, businessException);
+		this.validerCoordonnees(pseudo, nom, prenom, telephone, rue, codePostal, ville, businessException); 
+		this.validerMDP(user.getMotDePasse(),nouveauMDP, businessException);
 		
-		
-		return null;
+		if(!businessException.hasErreurs()) { 
+			
+			modifierProfilDAO.update_utilisateur(user);
+
+			} else { 
+				
+			throw businessException; 
+			}
+
+		return user;	
+
 	}
 
+	private void validerMDP(String motDePasse, String nouveauMDP, BusinessException businessException) {
+		
+		if(motDePasse.equals(nouveauMDP)) {
+			businessException.ajouterErreur("Le nouveau mot de passe ne doit pas être identique au mot de passe actuel.");
+		}
+	}
 	
-	private void validerCoordonnees(String pseudo, String nom, String prenom, String telephone, String rue, String codePostal,
-			String ville, String motDePasse, BusinessException businessException) {
 
-		if(pseudo ==null || pseudo.isBlank() || pseudo.isEmpty()){
+	private void validerCoordonnees(String pseudo, String nom, String prenom, String telephone, String rue,
+			String codePostal, String ville, BusinessException businessException) {
+
+		if (pseudo == null || pseudo.isBlank() || pseudo.isEmpty()) {
 			businessException.ajouterErreur("Le pseudo ne peut pas être vide");
 		}
-		
-		if(nom ==null || nom.isBlank() || nom.isEmpty()){
+
+		if (nom == null || nom.isBlank() || nom.isEmpty()) {
 			businessException.ajouterErreur("Le nom ne peut pas être vide");
 		}
-		
-		if(prenom ==null || prenom.isBlank() || prenom.isEmpty()){
+
+		if (prenom == null || prenom.isBlank() || prenom.isEmpty()) {
 			businessException.ajouterErreur("Le prénom ne peut pas être vide");
 		}
-		
-		if(!telephoneEstCorrect(telephone)){
+
+		if (!telephoneEstCorrect(telephone)) {
 			businessException.ajouterErreur("Le téléphone n'est pas valide");
 		}
-		
-		if(rue ==null || rue.isBlank() || rue.isEmpty()){
+
+		if (rue == null || rue.isBlank() || rue.isEmpty()) {
 			businessException.ajouterErreur("La rue ne peut pas être vide");
 		}
-		
-		if(!CodePostalCorrect(codePostal)){
+
+		if (!CodePostalCorrect(codePostal)) {
 			businessException.ajouterErreur("Le téléphone n'est pas valide");
+		}
+		
+		if (ville == null || ville.isBlank() || ville.isEmpty()) {
+			businessException.ajouterErreur("La ville ne peut pas être vide");
 		}
 		
 
+	}
+
+	private boolean CodePostalCorrect(String codePostal) {
 		
-		
-		
+	    String regex = "^(97|98)\\d{3}$|^\\d{5}$";
+	    Pattern pattern = Pattern.compile(regex);
+	    Matcher matcher = pattern.matcher(codePostal);
+	    
+	    return matcher.matches();
 	}
 
 	private boolean telephoneEstCorrect(String telephone) {
-		
-	    String regex = "^(\\+33|0)[1-9](\\d{2}){4}$";
-	    
-	    Pattern pattern = Pattern.compile(regex);
-	    
-	    Matcher matcher = pattern.matcher(telephone);
-	    
-	    return matcher.matches();
+
+		String regex = "^(\\+33|0)[1-9](\\d{2}){4}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(telephone);
+
+		return matcher.matches();
 	}
 
 	private void validerEmail(String email, BusinessException businessException) {
-		
+
 		if (email == null || email.isBlank() || email.isEmpty()) {
-			
+
 			// On ajouter l'erreur et on précise ce qu'elle renvoie
 			businessException.ajouterErreur("L'email ne peut pas être vide");
 		}
-		
-	    if (!emailEstCorrect(email)) {
-	        businessException.ajouterErreur("L'email n'est pas valide");
-	        return;
-	    }
-		
+
+		if (!emailEstCorrect(email)) {
+			businessException.ajouterErreur("L'email n'est pas valide");
+			return;
+		}
+
 	}
 
 	private boolean emailEstCorrect(String email) {
-		
+
 		// Cela permet de savoir ce que doit contenir l'email
-	    String regex = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-	    
-	    // On insère le regex dans un pattern
-	    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-	    
-	    // Grace à l'objet Matcher, on vérifie si que l'email correspond bien au pattern demandé.
-	    Matcher matcher = pattern.matcher(email);
-	    
-	    return matcher.matches();
-	
+		String regex = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+
+		// On insère le regex dans un pattern
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+		// Grace à l'objet Matcher, on vérifie si que l'email correspond bien au pattern
+		// demandé.
+		Matcher matcher = pattern.matcher(email);
+
+		return matcher.matches();
+
 	}
 
 	private void validerPseudo(String pseudo, BusinessException businessException) {
-		
+
 		if (pseudo == null || pseudo.trim().isEmpty()) {
-			
+
 			// On ajouter l'erreur et on précise ce qu'elle renvoie
 			businessException.ajouterErreur("Le pseudo n'est pas correct");
 		}
-		
+
 	}
-
-
-
 
 }
